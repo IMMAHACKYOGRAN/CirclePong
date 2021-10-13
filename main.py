1da7c8d21f2af249b4974e273d8f51ba14ab87ad
IMMAHACKYOGRAN <71002312+IMMAHACKYOGRAN@users.noreply.github.com>
2021-10-14 08:29:48 +1100

    It actually fucking works now

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ ctx = pygame.display.set_mode((width, height))
 bgcolour = (15, 17, 27)
 circlecolour = (255, 255, 255)
 paddlecolour = (245, 111, 91)
-multiplyer = 0.001
+multiplyer = 0.0001
 radius = 300
 # x * x + y * y < r then inside circle
 
 class Paddle:
     def __init__(self, r, paddlelength, paddleheight, colour, sens):
-        self.angle = 0
+        self.angle = 0 # POLAR COORDINATE
+        self.facing = 0 # WHERE IT IS LOOKING, NOT THE SAME AS WHERE IT IS
         self.r = r
         self.paddlelength = paddlelength
         self.paddleheight = paddleheight
@@ -22,36 +23,38 @@ class Paddle:
         self.sens = sens * multiplyer
         self.x = 0
         self.y = 0
-        self.x1 = 0
-        self.y1 = 0
-        self.x2 = 0
-        self.y2 = 0
         self.center = pygame.math.Vector2(width / 2, height / 2)
 
-    def findpoints(self):
-        print()
+    def rotatepoint(self, point, angle, origin):
+        x = math.cos(angle) * (point[0] - origin.x) - math.sin(angle) * (point[1] - origin.y) + origin.x
+        y = math.sin(angle) * (point[0] - origin.x) + math.cos(angle) * (point[1] - origin.y) + origin.y
+        return x, y
 
     def draw(self):
-        pygame.draw.polygon(ctx, self.colour, [(self.x, self.y), (self.x + self.paddlelength, self.y), (self.x + self.paddlelength, self.y + self.paddleheight), (self.x, self.y + self.paddleheight)])
+        print(self.angle)
+        pygame.draw.polygon(ctx, self.colour, 
+        [self.rotatepoint((self.x, self.y), self.angle, self.center), 
+        self.rotatepoint((self.x + self.paddlelength, self.y), self.angle, self.center), 
+        self.rotatepoint((self.x + self.paddlelength, self.y + self.paddleheight), self.angle, self.center), 
+        self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)])
 
         #pygame.draw.rect(ctx, self.colour, (self.x, self.y, self.paddlelength, self.paddleheight))
 
     def getCoordinates(self):
-        self.x = self.r * math.cos(self.angle) + self.center.x - self.paddlelength / 2
-        self.y = self.r * math.sin(self.angle) + self.center.y - self.paddleheight / 2
+        self.x = self.r * math.cos(self.angle / radius + radius) + self.center.x - self.paddlelength / 2
+        self.y = self.r * math.sin(self.angle / radius + radius) + self.center.yS
 
-player = Paddle(radius, 100, 25, paddlecolour, 5)
+player = Paddle(radius, 150, 25, paddlecolour, 5)
 
 def render():
     ctx.fill(bgcolour)
     pygame.draw.circle(ctx, circlecolour, player.center, radius, 3)
 
-def update():
+def update():    
     if player.angle >= player.sens:
-        player.angle -= player.sens 
+        player.angle -= player.sens
     else:
         player.angle = math.pi * 2
-
     player.getCoordinates()
     render()
     
@@ -66,3 +69,11 @@ while True:
             pygame.quit()
             exit(0)
     pygame.display.flip()
+
+""" 
+============={bibliography}============= 
+https://stackoverflow.com/questions/4465931/rotate-rectangle-around-a-point/13208761
+https://www.w3schools.com/python/python_tuples.asp
+https://www.pygame.org/docs/
+https://www.pygame.org/docs/ref/draw.html#pygame.draw.polygon
+"""
