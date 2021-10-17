9309f323606084414f1a72d7caba654c65dadfb2
IMMAHACKYOGRAN <71002312+IMMAHACKYOGRAN@users.noreply.github.com>
2021-10-18 08:53:47 +1100

    Early collision detection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from pygame.constants import K_a, K_d
 import pygame.gfxdraw
+import numpy as mathbutbetter
 
 pygame.init()
 width, height = 960, 720
@@ -23,7 +24,11 @@ class Ball:
         self.speed = 1
         self.r = r
         self.pos = pygame.math.Vector2(origin.x, origin.y)
-        self.vel = pygame.math.Vector2(0.1, 0)
+        self.vel = pygame.math.Vector2(0, -0.1)
+        self.left = self.pos.x - self.r / 2
+        self.right = self.pos.x + self.r / 2
+        self.top = self.pos.y + self.r / 2
+        self.bottom = self.pos.y - self.r / 2
         
     def getVels(self):
         pass
@@ -37,9 +42,7 @@ class Ball:
     
     def inbounds(self):
         if math.sqrt(math.pow(self.pos.x - player.center.x, 2) + math.pow(self.pos.y - player.center.y, 2)) > radius:
-            print("outside")
-        else:
-            print("inside")
+            pass
 
 class Paddle:
     def __init__(self, r, paddlelength, paddleheight, colour, sens):
@@ -52,10 +55,29 @@ class Paddle:
         self.x = 0
         self.y = 0
         self.center = pygame.math.Vector2(width / 2, height / 2)
+        self.points = [
+            self.rotatepoint((self.x, self.y), self.angle, self.center), 
+            self.rotatepoint((self.x + self.paddlelength, self.y), self.angle, self.center), 
+            self.rotatepoint((self.x + self.paddlelength, self.y + self.paddleheight), self.angle, self.center), 
+            self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)]
+
+    def getCollision(self):
+        #update points
+        self.points = [
+            self.rotatepoint((self.x, self.y), self.angle, self.center), 
+            self.rotatepoint((self.x + self.paddlelength, self.y), self.angle, self.center), 
+            self.rotatepoint((self.x + self.paddlelength, self.y + self.paddleheight), self.angle, self.center), 
+            self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)]
+
+        #find closest vertex to circle
+        pointDists = []
+        for point in self.points:
+            dist = math.sqrt(math.pow(ball.pos.x - point[0], 2) + math.pow(ball.pos.y - point[1], 2))
+            pointDists.append(dist)
         
-    def getCollision():
-        #if ball.pos.x
-        pass
+        closestPoint = min(pointDists)
+        #if distance to closest point on edge is less than ball.r {return true}
+            #print("BALL HAS COLLIDED WITH THE PONG")
 
     def move(self):
         if keys[0] == True:
@@ -63,6 +85,7 @@ class Paddle:
                 self.angle -= self.sens
             else:
                 self.angle = math.pi * 2 - self.sens
+
         if keys[1] == True:
             if self.angle <= math.pi * 2:
                 self.angle += self.sens
@@ -82,16 +105,18 @@ class Paddle:
         self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)], self.colour)
 
         pygame.draw.polygon(ctx, self.colour, 
-        [self.rotatepoint((self.x, self.y), self.angle, self.center), #point 1
-        self.rotatepoint((self.x + self.paddlelength, self.y), self.angle, self.center), #point 2
-        self.rotatepoint((self.x + self.paddlelength, self.y + self.paddleheight), self.angle, self.center), #point 3 
-        self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)]) #point 4
+        [self.rotatepoint((self.x, self.y), self.angle, self.center), #top left point
+        self.rotatepoint((self.x + self.paddlelength, self.y), self.angle, self.center), #top right point
+        self.rotatepoint((self.x + self.paddlelength, self.y + self.paddleheight), self.angle, self.center), #bottom right point 
+        self.rotatepoint((self.x, self.y + self.paddleheight), self.angle, self.center)]) #bottom left point
+
+        #pygame.draw.aaline(ctx, (255, 255, 0), ball.pos, (self.closestPoint, self.closestPoint))
 
     def getCoordinates(self): # Converts Polar coordinate to cartiesian.
         self.x = self.r * math.cos(self.angle / radius + radius) + self.center.x - self.paddlelength / 2
         self.y = self.r * math.sin(self.angle / radius + radius) + self.center.y
 
-player = Paddle(radius, 150, 25, paddlecolour, 10)
+player = Paddle(radius, 150, 25, paddlecolour, 2)
 ball = Ball(player.center, 10)
 
 def render():
@@ -105,6 +130,7 @@ def update():
     player.move()
     ball.move()
     ball.inbounds()
+    player.getCollision()
     render()
 
 while True:
@@ -136,4 +162,10 @@ https://www.w3schools.com/python/python_tuples.asp
 https://www.pygame.org/docs/
 https://www.pygame.org/docs/ref/draw.html#pygame.draw.polygon
 https://www.youtube.com/watch?v=4y_nmpv-9lI
+https://www.reddit.com/r/gamedev/comments/xtry1/circlepolygon_collison_using_sat/
+https://www.youtube.com/watch?v=59BTXB-kFNs
+https://www.youtube.com/watch?v=-EsWKT7Doww
+https://www.youtube.com/watch?v=RBya4M6SWwk
+https://stackoverflow.com/questions/3499026/find-a-minimum-value-in-an-array-of-floats
+https://www.w3schools.com/python/python_arrays.asp
 """
